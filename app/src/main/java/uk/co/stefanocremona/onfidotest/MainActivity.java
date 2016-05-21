b528a2041f3d579f250d5ad0e4d209d9050f9682
Stefano Cremona <stefano.cremona@gmail.com>
2016-05-21 19:12:03 +0100

    Add Layout inflate by xml.

--- app/src/main/java/uk/co/stefanocremona/onfidotest/MainActivity.java
+++ app/src/main/java/uk/co/stefanocremona/onfidotest/MainActivity.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import uk.co.stefanocremona.mycomiclibrary.tasks.ComicTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final Activity myActivity = this;
-        setContentView(uk.co.stefanocremona.mycomiclibrary.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        new uk.co.stefanocremona.mycomiclibrary.tasks.ComicTask(this).execute(new String[]{"http://xkcd.com/info.0.json"});
+        ComicTask myTask = new ComicTask(this);
+        myTask.execute(new String[]{"http://xkcd.com/info.0.json"});
     }
 }
